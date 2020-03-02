bcb78c71d584af2658926bcebdfbd639694cf8fb
Harry <profawesum@gmail.com>
2020-03-02 14:21:49 +1300

    One type of gem is working

---
+++ BugsLife/Assets/Scripts/Player/CollectibleManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CollectibleManager : MonoBehaviour
+{
+
+    public int basicGemCount;
+
+    public Text gemCountText;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Gem") {
+            Destroy(other.gameObject);
+            updateGemCount();
+        }
+    }
+
+    public void updateGemCount() {
+        basicGemCount++;
+        gemCountText.text = "Gems: " + basicGemCount.ToString();
+    }
+
+}

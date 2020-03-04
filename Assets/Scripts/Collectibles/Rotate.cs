edfa5bfe4eafff801e90ee27005e93a81c79c212
Harry <profawesum@gmail.com>
2020-03-05 12:04:30 +1300

    Updates to collectible manager

--- Assets/Scripts/Collectibles/Rotate.cs
+++ Assets/Scripts/Collectibles/Rotate.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Rotate : MonoBehaviour
 {
 
-    public float rotateSpeed;
+    public float rotateSpeed = 50;
     public GameObject[] jellybeans;
 
     // Update is called once per frame
     void Update()
     {
+        //find all the jellybeans
         jellybeans = GameObject.FindGameObjectsWithTag("Gem");
+        
+        //for each jellybean rotate it
         foreach (GameObject go in jellybeans)
         {
             go.transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed);
--- Assets/Scripts/Player/CollectibleManager.cs
+++ Assets/Scripts/Player/CollectibleManager.cs
@@ -10,7 +10,29 @@ public class CollectibleManager : MonoBehaviour
     public int basicGemCount;
     public Text gemCountText;
 
-    
+    //counts for the main Collectible
+    public int mainCollectibleCount;
+    public Text mainCollectibleText;
+
+    //counts for the ants found
+    public int antCount;
+    public Text antText;
+
+    //the amount of jellybeans left in the level
+    public GameObject[] jellyBean;
+
+
+    private void Update()
+    {
+        //find the amount of jellybeans left
+        jellyBean = GameObject.FindGameObjectsWithTag("Gem");
+
+        if (jellyBean.Length == 0) { 
+            //TODO:
+            //do some stuff to spawn in a main collectible
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         //colliding with a gem calls the function to update the amount of gems the player has
@@ -18,8 +40,32 @@ public class CollectibleManager : MonoBehaviour
             Destroy(other.gameObject);
             updateGemCount();
         }
+
+        //colliding with another ant collects them
+        if (other.tag == "Ant")
+        {
+            Destroy(other.gameObject);
+            updateAntCount();
+        }
+        //collides with the main collectible
+        if (other.tag == "mainCollectible") {
+            Destroy(other.gameObject);
+            updateMainCollectible();
+        }
     }
 
+
+    //updates the main collectible
+    public void updateMainCollectible() { 
+    
+
+    }
+
+    //increase the amount of ants the player has found
+    public void updateAntCount() { 
+    
+
+    }
 
     //increase the value of the gems
     public void updateGemCount() {

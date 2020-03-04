a4c25f9071b914fb1c777c65fa24245c7a643055
Harry <profawesum@gmail.com>
2020-03-05 10:59:29 +1300

    PLayer Animations
    Player anims are working

---
+++ Assets/Scripts/Player/PlayerAnimController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimController : MonoBehaviour
+{
+
+
+    public Animator animator;
+    [SerializeField] PlayerMove playerMove;
+    public CharacterController controller;
+
+    private void Start()
+    {
+        controller = GetComponentInParent<CharacterController>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.velocity != Vector3.zero)
+        {
+            if (playerMove.climbable)
+            {
+                animator.SetBool("Climbing", true);
+                animator.SetBool("Running", false);
+            }
+            else
+            {
+
+                animator.SetBool("Climbing", false);
+                animator.SetBool("Running", true);
+            }
+        }
+        else {
+            animator.SetBool("Climbing", false);
+            animator.SetBool("Running", false);
+        }
+
+
+    }
+}

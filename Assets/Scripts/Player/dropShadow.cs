675cc89361c51ef730869fe2e91c1c72f1bc2b94
Harry <profawesum@gmail.com>
2020-03-09 09:11:13 +1300

    Work on player shadow

---
+++ Assets/Scripts/Player/dropShadow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropShadow : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hit;
+
+        //raycast downwards
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+        {
+            this.transform.position = hit.point;
+            //draw the raycast line
+            Debug.DrawLine(transform.position, hit.point, Color.cyan);
+        }
+    }
+}
